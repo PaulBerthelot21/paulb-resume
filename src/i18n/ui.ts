cc37073ca6fe09f8a4ceb41ad7617aa337b6aaa0
Paul Berthelot <paul.berthelot@cordongroup.com>
2024-11-07 17:11:18 +0100

    Starting the translation of the index.astro

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,8 +8,54 @@ export const showDefaultLang = false;
 
 export const ui = {
     fr: {
-        'nav.home': 'Accueil',
-        'nav.about': 'À propos',
+        'index.home' : 'Accueil',
+
+        'index.start.status' : 'Alternant en M2 chez Cordon Group',
+        'index.start.post-office' : 'Développeur Full Stack',
+        'index.start.location' : 'Saint-Malo',
+        'index.start.linkedin-connect' : 'Se connecter avec LinkedIn',
+
+        'index.about.title' : 'À propos',
+        'index.about.description' : 'Je m\'appelle Paul Berthelot, et je suis un développeur full-stack de 22 ans. ' +
+            'Sur ce portfolio, tu découvriras mes projets, mes compétences techniques, et mon approche en matière de ' +
+            'développement web.',
+
+        'index.experience.title' : 'Expérience',
+        'index.experience.cordon.title' : 'Développeur Full Stack',
+        'index.experience.cordon.company' : 'Cordon Group',
+        'index.experience.cordon.date' : 'Sep 2022 - Aujourd\'hui',
+        'index.experience.cordon.task' : 'Développement d\'une application web multi-modules pour la gestion de projet et la qualité.',
+
+        'index.experience.ghre.title' : 'Développeur Full Stack',
+        'index.experience.ghre.company' : 'Groupement Hospitalier Rance-Emeraude',
+        'index.experience.ghre.date' : 'Stage - 4 mois',
+        'index.experience.ghre.task' : 'Refonte et harmonisation graphique des applications développer en interne du groupement hospitalier.',
+        'index.experience.ghre.task-two' : 'Création de script shell pour l\'éxécution de requête SQL',
+        'index.experience.ghre.task-three' : 'Déploiement sur serveur et sur Active Directory pour les utilisateurs',
+
+        'index.education.title' : 'Formation',
+
+        'index.education.m2.title' : 'Manager de solutions digitales et data (Bac +5)',
+        'index.education.m2.school' : 'ENI Ecole Informatique',
+        'index.education.m2.date' : 'Aou 2024 - Nov 2025',
+
+        'index.education.m1.title' : 'Concepteur Développeur Application (Bac +4)',
+        'index.education.m1.school' : 'ENI Ecole Informatique',
+        'index.education.m1.date' : 'Sep 2022 - Aou 2024',
+
+        'index.education.bts.title' : 'BTS SIO option SLAM (Bac +2)',
+        'index.education.bts.school' : 'Greta Saint-Malo',
+        'index.education.bts.date' : 'Aou 2021 - Jui 2022',
+        'index.education.bts.more-info' : 'BTS Services Informatiques aux Organisations option Solutions Logicielles et' +
+            ' Applications Métiers (SIO SLAM)',
+
+        'index.projects.title' : 'Projets',
+
+        'index.skills.title' : 'Compétences',
+        'index.skills.front-end' : 'Front-end',
+        'index.skills.back-end' : 'Back-end',
+        'index.skills.database' : 'Base de données',
+        'index.skills.other' : 'Autres',
 
         '404.head.title' : '404 — Page non trouvée',
         '404.head.description' : 'Erreur 404 — Cette page n\'a pas été trouvé',
@@ -21,8 +67,52 @@ export const ui = {
         'footer.rights': 'Tous droits réservés',
     },
     en: {
-        'nav.home': 'Home',
-        'nav.about': 'About',
+        'index.home' : 'Home',
+        'index.start.status' : 'M2 apprentice at Cordon Group',
+        'index.start.post-office' : 'Full Stack Developer',
+        'index.start.location' : 'Saint-Malo',
+        'index.start.linkedin-connect' : 'Connect with LinkedIn',
+
+        'index.about.title' : 'About',
+        'index.about.description' : 'My name is Paul Berthelot, and I am a 22-year-old full-stack developer. ' +
+            'On this portfolio, you will discover my projects, my technical skills, and my approach to web development.',
+
+        'index.experience.title' : 'Experience',
+        'index.experience.cordon.title' : 'Full Stack Developer',
+        'index.experience.cordon.company' : 'Cordon Group',
+        'index.experience.cordon.date' : 'Sep 2022 - Today',
+        'index.experience.cordon.task' : 'Development of a multi-module web application for project management and quality.',
+
+        'index.experience.ghre.title' : 'Full Stack Developer',
+        'index.experience.ghre.company' : 'Groupement Hospitalier Rance-Emeraude',
+        'index.experience.ghre.date' : 'Internship - 4 months',
+        'index.experience.ghre.task' : 'Redesign and graphic harmonization of the applications developed internally by the hospital group.',
+        'index.experience.ghre.task-two' : 'Creation of shell script for the execution of SQL queries',
+        'index.experience.ghre.task-three' : 'Deployment on server and on Active Directory for users',
+
+        'index.education.title' : 'Education',
+
+        'index.education.m2.title' : 'Manager of digital solutions and data (Bac +5)',
+        'index.education.m2.school' : 'ENI Ecole Informatique',
+        'index.education.m2.date' : 'Aug 2024 - Nov 2025',
+
+        'index.education.m1.title' : 'Application Developer Designer (Bac +4)',
+        'index.education.m1.school' : 'ENI Ecole Informatique',
+        'index.education.m1.date' : 'Sep 2022 - Aug 2024',
+
+        'index.education.bts.title' : 'BTS SIO option SLAM (Bac +2)',
+        'index.education.bts.school' : 'Greta Saint-Malo',
+        'index.education.bts.date' : 'Aug 2021 - Jul 2022',
+        'index.education.bts.more-info' : 'BTS Services Informatiques aux Organisations option Solutions Logicielles et' +
+            ' Applications Métiers (SIO SLAM)',
+
+        'index.projects.title' : 'Projects',
+
+        'index.skills.title' : 'Skills',
+        'index.skills.front-end' : 'Front-end',
+        'index.skills.back-end' : 'Back-end',
+        'index.skills.database' : 'Database',
+        'index.skills.other' : 'Other',
 
         '404.head.title' : '404 — Page not found',
         '404.head.description' : 'Error 404 — This page was not found',
